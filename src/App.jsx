b2fa8bf6fa1ff93521c1ba5d8aab002d1c52e84d
HakimaNoori <hakima.noori.1379@gmail.com>
2025-02-15 22:12:05 +0430

    feat(Interior Design Website): Add project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Welcome to the Interior Design Website</h1>
+        </div>
+    );
+}
+
+export default App;
